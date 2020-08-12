1ceffa4b779395f92eaf67114b7a871bdf456277
unknown <adityaparsad01@gmail.com>
2020-08-12 19:33:21 +0530

    first commit

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ answer, question, qno, logo }) => {
   return (
     <div key={qno} className="col-8 col-sm-6 col-md-4 col-lg-3 col-xl-3 my-3">
-      <div className="card shadow" style={{ width: "18rem" }}>
+      <div className=" m-auto card shadow" style={{ width: "18rem" }}>
         <img src={logo} className="card-img-top" alt={logo} />
         <div className="card-body">
           <h5 className="card-title text-capitalize">{question}</h5>
